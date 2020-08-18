cc8deee220d1b9a0766aa6fc4c959d673338cd54
Mohammed Quazi <61766236+mquazi@users.noreply.github.com>
2020-08-17 18:06:14 -0600

    Add files via upload

---
+++ australia/ausauto.R
@@ -0,0 +1,113 @@
+
+
+inddat<-read.csv("/Users/quazi/Desktop/cric/australia.csv",header=T)
+head(inddat)
+str(inddat)
+ttt<-nrow(inddat)/2   #total teams
+i1<-inddat[1:ttt,]
+i1
+i2<-inddat[(ttt+1):(ttt+ttt),]
+i2
+
+NA2mean <- function(x) replace(x, is.na(x), mean(x, na.rm = TRUE))
+i1<-replace(i1[,3:ncol(i1)], TRUE, lapply(i1[,3:ncol(i1)], NA2mean))
+i2<-replace(i2[,3:ncol(i2)], TRUE, lapply(i2[,3:ncol(i2)], NA2mean))
+i2
+i1
+
+i1<-cbind(inddat[1:ttt,2],i1)
+i2<-cbind(inddat[1:ttt,2],i2)
+i1
+i1<-i1[-2,]
+i2<-i2[-2,]
+head(i1)
+head(i2)
+m<-(ttt-1)   #number of teams
+n<-11    ##always 11
+
+ie<-rbind(i1[1:m,],i2[1:m,])
+ie
+
+g<-as.data.frame(t(ie[,-1]))
+g$myfactor <- factor(row.names(g))
+
+g$myfactor<-str_remove_all(g$myfactor, "[V0123456789]")
+k<-as.numeric(factor(g$myfactor))
+g<-cbind(g,k)
+g$myfactor <- factor(g$myfactor)
+
+
+g2<-g[order(g$myfactor),]
+g2[g2 == 0] <- 1
+g2
+
+
+autoaus<-function(){
+        select<-strata(g2,stratanames=c("myfactor"),size=c(2,1,4,2,1,1),method = "srswor")
+        #table(select$myfactor)
+        hs<-getdata(g2,select)[,1:m]
+        ave<-getdata(g2,select)[,(m+1):(m+m)]
+        #hs
+        #ave
+        
+        
+        
+        l2<-NULL
+        for (i in 1:n){
+                r1<-hs[i,]
+                r2<-ave[i,]
+                l1<-ifelse((r1<r2) | (r1==r2), max(r1,r2+15), r1)
+                l2[i]<-list(l1)
+        }
+        #l2
+        l3<-data.frame(matrix(unlist(l2), nrow=n, byrow=T))
+        #l3
+        
+        l4<-NULL
+        l5<-NULL
+        for(i in 1:n){
+                for (j in 1:m){
+                        
+                        a<-l3[i,j]
+                        b<-ave[i,j]
+                        score<-f(b,a)
+                        l4[j]<-(score)
+                }
+                l5[i]<-list(l4)
+        }
+        
+        score<-data.frame(matrix(unlist(l5), nrow=n, byrow=T))
+        #score
+        #ave
+        #l6<-ifelse(is.na(score), ave, r1)
+        #is.na(score)
+        score<-ceiling(score)
+        scores<-colSums(score)
+        #scores
+        list(scores=scores)
+}
+
+
+autoaus()
+
+
+aussims<-replicate(5,autoaus())
+aussims[[1]][5]
+
+aa<-data.frame(matrix(unlist(aussims), nrow=length(aussims), byrow=T))
+aa
+colnames(aa)<-c("v Afghanistan","v Bangladesh",   "v England",
+                "v Hong Kong","v India","v Ireland","v Kenya","v Netherlands", "v New Zealand" ,
+                "v Pakistan", "v South Africa", "v Sri Lanka", "v U.A.E.", 
+                "v West Indies", "v Zimbabwe")
+aa
+ia
+ed
+aa[,5]<ia[,2]
+aa[,3]<ed[,2]
+aa[,5]
+ia[,2]
+
+aussims
+
+
